c8b087b1bf3e988170acc512100556240cb68b02
Frank <fmurga@unsa.edu.pe>
2022-05-09 11:03:59 -0500

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div>
         {/* Media query:: md:w-2/3:::Apartir de 768px toma el 66% de la pantalla */}
-      <h1 className='font-black text-5xl text-center md::w-2/3 mx-auto'>Seguimiento pacientes  
+      <h1 className='font-black text-5xl text-center md:w-2/3 mx-auto'>Seguimiento pacientes  
           <span className='text-indigo-600'>Veterinaria</span>
           </h1>
     </div>
